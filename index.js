28f5c013f554d280980e92a4840116d71f20dc7b
LeandroOnline <leandrosavat@gmail.com>
2024-02-29 16:04:50 -0300

    change exec for spawn

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 app.get("/api", (req, res) => {
-  exec("npx playwright test", (error, stdout, stderr) => {
-    try {
-      const result = JSON.parse(stdout);
-      res.json(result.suites);
-      console.log('Test done!')
-    } catch (e) {
-      res.status(500).json({ error: `Error parsing stdout to JSON: ${e}, stdout: ${stdout}` });
-      console.log('Test failed!')
-    }
+  const child = spawn("npx", ["playwright", "test"], {
+    stdio: ["inherit", "pipe", "pipe"],
+  });
+
+  let output = "";
+
+  child.stdout.on("data", (data) => {
+    output += data.toString();
+  });
+
+  child.stderr.on("data", (data) => {
+    console.error(`Error from child process: ${data}`);
+  });
+
+  child.on("exit", (code, signal) => {
+    // if (code === 0) {
+      try {
+        const result = JSON.parse(output);
+        res.json(result.suites);
+        console.log("Test done!");
+      } catch (e) {
+        res.status(500).json({ error: `Error parsing output to JSON: ${e}` });
+        console.error("Failed to parse test results:", output);
+      }
+    // } else {
+    //   res.status(500).json({ error: `Playwright test failed with code ${code}` });
+    //   console.error("Playwright test failed:", output);
+    // }
   });
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.listen( 3000, () => console.log("Server running"));
+app.listen(3000, () => console.log("Server running"));
 
 module.exports = app;
