8999b213e0267b7b0f20af37c5ec0cbf2bc1f8d8
LeandroOnline <leandrosavat@gmail.com>
2024-03-13 15:27:06 -0300

    feat: test and review settings

---
+++ e2e/PlaymakersTestSuite/layout.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "@playwright/test";
+import url from "./url";
+
+test("layout", async ({ page }) => {
+  await page.goto(url);
+  await expect(page.getByTestId("infoBanner")).toBeVisible();
+  await expect(page.getByTestId("nav")).toBeVisible();
+  await expect(page.getByTestId("preFooter")).toBeVisible();
+  await expect(page.getByTestId("flowbite-footer")).toBeVisible();
+});
---
+++ e2e/Superwebpros/test-1.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.superwebpros.com/');
+  await page.getByRole('link', { name: 'Bonuses Included' }).click();
+  await expect(page.getByText('Pro-fessional WP Theme Bundle')).toBeVisible();
+});
