53da6275b2b62aefc30f21c9ac9f6698157543cc
LeandroOnline <leandrosavat@gmail.com>
2024-03-15 11:12:11 -0300

    feat: new folder and index barrel

--- e2e/Playmakers/front/spec/layout.spec.ts
+++ e2e/Playmakers/front/spec/layout.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import url from "../../config/frontUrl";
 
 export default function createTest() {
-  test("layout", async ({ page }) => {
+  test("loads", async ({ page }) => {
     await page.goto(url);
     await expect(page.getByTestId("infoBanner")).toBeVisible();
     await expect(page.getByTestId("nav")).toBeVisible();
