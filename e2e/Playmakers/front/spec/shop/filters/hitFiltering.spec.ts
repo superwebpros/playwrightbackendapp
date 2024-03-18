e587e11fb53b125f922ed0ce6dff6279598dce89
LeandroOnline <leandrosavat@gmail.com>
2024-03-18 17:29:53 -0300

    feat: test for filtering

--- e2e/Playmakers/front/spec/shop/filters/hitFiltering.spec.ts
+++ e2e/Playmakers/front/spec/shop/filters/hitFiltering.spec.ts
@@ -7,6 +7,15 @@ export default function createTest() {
     await expect(page.getByTestId("container-filters")).toBeVisible();
     await expect(page.getByRole("button", { name: "Gender" })).toBeVisible();
     await page.getByRole("button", { name: "Gender" }).click();
-    await page.getByRole('button', { name: 'Women' }).click();
+    await page.getByRole("button", { name: "Men", exact: true }).click();
+    const links = await page
+      .getByTestId("infiniteHits")
+      .locator(".ais-InfiniteHits")
+      .locator(".ais-InfiniteHits-list")
+      .getByRole("link")
+      .allTextContents();
+      console.log(links);
+    const linksNames = links.filter((link) => link.includes("Women's"));
+    await expect(linksNames.length).toBe(0);
   });
 }
