d47b957cc1000516220da66da1074f2f03f054c1
LeandroOnline <leandrosavat@gmail.com>
2024-03-20 15:02:05 -0300

    update: navigate extension

--- e2e/Playmakers/front/spec/shop/breadCrumbs/navigate.spec.ts
+++ e2e/Playmakers/front/spec/shop/breadCrumbs/navigate.spec.ts
@@ -8,6 +8,30 @@ export default function createTest() {
     await page.getByRole("link", { name: "〉Women" }).click();
     await page.waitForLoadState();
     await expect(page.url()).toBe(url + "/collections/women");
+    await page.getByRole("link", { name: "〉Collections" }).click();
+    await page.waitForLoadState();
+    await expect(page.url()).toBe(url + "/collections/all");
+  });
+
+  test("go to product and navigate from breadcrumbs ", async ({ page }) => {
+    await page.goto(url + "/collections/all");
+    await page.waitForTimeout(2000);
+
+    await page
+      .getByTestId("infiniteHits")
+      .locator(".ais-InfiniteHits")
+      .locator(".ais-InfiniteHits-list")
+      .locator(".ais-InfiniteHits-item")
+      .first()
+      .click();
+    await page.waitForLoadState();
+    await expect(
+      page.locator("form").filter({ hasText: "Add to Cart" }).locator("svg")
+    ).toBeVisible();
+    await expect(
+      page
+        .getByRole('link', { name: '〉Collections' })
+    ).toBeVisible();
     await page.getByRole('link', { name: '〉Collections' }).click();
     await page.waitForLoadState();
     await expect(page.url()).toBe(url + "/collections/all");
