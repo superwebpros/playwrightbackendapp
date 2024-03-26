c5702c332d5d173136b0aa4f88bd0211a0123538
LeandroOnline <leandrosavat@gmail.com>
2024-03-26 12:20:59 -0300

    fix: createFolder script

--- scripts/createE2EFolder.cjs
+++ scripts/createE2EFolder.cjs
@@ -1,8 +1,11 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
+
+// Directorio raíz del proyecto
+const rootDir = path.resolve(__dirname, "..");
 
 // Directorio de la carpeta e2e
-const e2eFolder = path.join(__dirname, "e2e");
+const e2eFolder = path.join(rootDir, "e2e");
 
 // Crear la carpeta e2e si no existe
 if (!fs.existsSync(e2eFolder)) {
@@ -10,17 +13,19 @@ if (!fs.existsSync(e2eFolder)) {
 }
 
 // Ruta del archivo index.js dentro de la carpeta e2e
-const indexPath = path.join(e2eFolder, "imports.list.ts");
+const indexPath = path.join(e2eFolder, "imports.list.js");
 
 // Crear el archivo index.js si no existe
 if (!fs.existsSync(indexPath)) {
   fs.writeFileSync(
     indexPath,
     `
-    // sellect the project you want to test
+    // select the project you want to test
     import { playmakers } from "@superwebpros/testsuite";
     import { test, expect } from "@playwright/test";
     // run the test
     test.describe("Imports", playmakers);`
   );
 }
+
+console.log("¡La instalación de la librería se completó correctamente!");
